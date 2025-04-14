7e324db0f08e6779d91efc32d1401e89c77e4f44
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-04-14 10:57:35 +0530

    init commit

---
+++ labely.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Labely
+ * Plugin URI:  
+ * Description: A custom plugin for Labely functionality.
+ * Version: 1.0.0
+ * Author: Your Name
+ * Author URI: https://www.linkedin.com/in/abhishek-kushwaha-5a3a49302/
+ * License: GPL2
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Your plugin code starts here.
